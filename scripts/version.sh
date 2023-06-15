9bca8c594ee0073124fe669eb41f655d6aac2338
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2023-06-15 04:18:39 +0100

    hack to fix version in actions?

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if git --version &>/dev/null; then
+    [[ ! -z ${CI} ]] && git config --global --add safe.directory /__w/rb3e-launcher/rb3e-launcher
     cat > ./src/version.h <<EOF
 // This file will be auto generated during building, and should not be modified manually
 #define BUILD_TAG "$(git describe --always --match \"tag\" --dirty)"
